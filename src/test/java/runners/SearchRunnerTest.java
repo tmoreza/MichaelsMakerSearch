316b0c51e548a37831841cabe83f71e3d9c3b4f2
tmoreza <165936871+tmoreza@users.noreply.github.com>
2024-04-03 15:31:01 -0500

    Add files via upload

---
+++ src/test/java/runners/SearchRunnerTest.java
@@ -0,0 +1,23 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features",
+		glue = "steps",
+		tags = "@Regression",
+		monochrome = true,
+		plugin = {
+				"html:target/cucumber-html-report", 
+				"json:target/cucumber.json",
+				"pretty:target/cucumber-pretty.txt",
+				"usage:target/cucumber-usage.json", 
+				"junit:target/cucumber-results.xml"}, 
+		snippets = SnippetType.CAMELCASE)
+public class SearchRunnerTest {
+}
